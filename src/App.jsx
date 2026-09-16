d441d24c28d58cb258d5b99b8dc8beea38f82d69
Amr Ahmed <amrahmed09951@gmail.com>
2026-09-16 06:24:06 +0300

    fetching weather data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 function App() {
   const [selected, setSelected] = useState(new Date);
+  const [weatherData, setWeatherData] = useState(null);
+  useEffect(() => {
+    const url = "https://api.open-meteo.com/v1/forecast?latitude=30.0626&longitude=31.2497&current=temperature_2m,wind_speed_10m" 
+    fetch(url).then(response => response.json()).then(data => {setWeatherData(data);console.log(data)})
+    .catch(error => console.error("Error fetching weather:", error));
+  }, [])
   return (
     <>
       <div className="h-screen w-full bg-cover bg-center bg-no-repeat bg-[url(/hero-bg.png)]">
-        <div className="text-white backdrop-blur-sm bg-black/10 border border-white/20 shadow-xl w-100 rounded-xl p-6">
-          <div>
+        <div className="p-9 flex">
+          <div className="text-white/80 backdrop-blur-sm bg-black/10 border border-white/20 shadow-xl w-90 rounded-xl p-6">
             <div className="mb-2">
               <span className="text-lg font-medium text-white/90">Calender</span>
             </div>
@@ -32,6 +38,11 @@ function App() {
                   disabled: "[&>button]:!text-white/20",
                 }}
               />
+            </div>
+          </div>
+          <div className="backdrop-blur-sm border border-white/20 bg-black/10 text-white/80 shadow-xl w-200 rounded-xl p-6 ml-10">
+            <div>
+              <span className="text-lg font-medium text-white/90">My Tasks</span>
             </div>
           </div>
         </div>
