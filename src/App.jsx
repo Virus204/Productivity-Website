70c130fd07e9790f75cef0039af2afb1a73d7801
Amr Ahmed <amrahmed09951@gmail.com>
2026-09-19 05:42:19 +0300

    adding more lines of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { FadeArc } from "@/components/ui/fade-arc";
 function App() {
   const [selected, setSelected] = useState(new Date);
   const [weatherData, setWeatherData] = useState(null);
   useEffect(() => {
     const url = "https://api.open-meteo.com/v1/forecast?latitude=30.0626&longitude=31.2497&current=temperature_2m,wind_speed_10m" 
-    fetch(url).then(response => response.json()).then(data => {setWeatherData(data);console.log(data)})
+    fetch(url).then(response => response.json()).then(data => {setWeatherData(data);})
     .catch(error => console.error("Error fetching weather:", error));
   }, [])
   return (
     <>
-      <div className="h-screen w-full bg-cover bg-center bg-no-repeat bg-[url(/hero-bg.png)]">
+      <div className="h-screen w-full bg-cover bg-center bg-no-repeat bg-[url(/hero-bg2.png)]">
         <div className="p-9 flex">
           <div className="text-white/80 backdrop-blur-sm bg-black/10 border border-white/20 shadow-xl w-90 rounded-xl p-6">
             <div className="mb-2">
@@ -32,12 +33,23 @@ function App() {
                   day_button:"w-9 h-9 mx-auto rounded-full text-md text-white/80 hover:bg-white/10",
                 }} 
                 modifiersClassNames={{
-                  selected: " [&>button]:!bg-violet-500 [&>button]:!text-white",
+                  selected: " [&>button]:!bg-[#34117E] [&>button]:!text-white",
                   today: "font-semibold",
                   outside: "[&>button]:!text-white/20",
                   disabled: "[&>button]:!text-white/20",
                 }}
               />
+            </div>
+          </div>
+          <div className="backdrop-blur-sm border border-white/20 bg-black/10 text-white/80 shadow-xl w-100 rounded-xl p-6 ml-10">
+            <div className="flex items-center justify-center w-full min-h-[200px]">
+              {weatherData ?(
+                <div>
+                  <h2>{weatherData.current.temperature_2m} {weatherData.current_units.temperature_2m}</h2>
+                </div>
+              ):(
+                <FadeArc className="size-20 text-[#34117E]"/>
+              )}
             </div>
           </div>
           <div className="backdrop-blur-sm border border-white/20 bg-black/10 text-white/80 shadow-xl w-200 rounded-xl p-6 ml-10">
@@ -47,6 +59,7 @@ function App() {
           </div>
         </div>
       </div>
+      {console.log(weatherData)}
     </>
   )
 }
